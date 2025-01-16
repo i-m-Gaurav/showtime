9180c4abd8e10ecba826ef09c4156369a3cdec0f
Gaurav kumar <indiagauravkumar@gmail.com>
2025-01-16 16:48:04 +0530

    implement user creation in NextAuth sign-in callback with Prisma

--- auth.ts
+++ auth.ts
@@ -1,7 +1,34 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaClient } from "@prisma/client";
 
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
- 
+const prisma = new PrismaClient();
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
-})
+  callbacks: {
+    async signIn({ user }) {
+      // Save user data to the database
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { email: user.email! },
+        });
+
+        if (!existingUser) {
+          await prisma.user.create({
+            data: {
+              name: user.name!,
+              email: user.email!,
+              role : "user"
+            },
+          });
+        }
+      } catch (error) {
+        console.error("Error saving user to database:", error);
+        return false; // Prevent sign-in if there's an error
+      }
+
+      return true; // Allow sign-in
+    },
+  },
+});
