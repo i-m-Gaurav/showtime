dd4a960fbcdf29c75f7450367997d201d0b38b26
Gaurav kumar <indiagauravkumar@gmail.com>
2025-01-30 10:28:26 +0530

    add middleware for authentication, link to browse shows, and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 
 // import AllShows from "@/components/AllShows";
+import Link from 'next/link';
 export default function Home() {
   return (
     <div>
       {/* <AllShows/> */}
+      <Link href = '/shows'>Browse shows</Link>
     </div>
   );
 }
+
+
