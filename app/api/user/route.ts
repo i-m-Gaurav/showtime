a0942ad7f6126d18dc787ed2000d86ea0ffb6bf9
Gaurav kumar <indiagauravkumar@gmail.com>
2025-02-06 02:54:00 +0530

    feat: add API routes for seat management and user retrieval

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,15 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import {prisma} from '@/lib/prisma';
 
 export async function GET(req:NextRequest){
+
+    //Getting the user email for something, i don't know. probably delete this route later.
     
     const email = req.nextUrl.searchParams.get('email');
-
     const user = await prisma.user.findUnique({
         where : {
-            email,
+            email: email ?? undefined,
         }
     });
-
     return NextResponse.json(user, {status: 200});
-
 }
