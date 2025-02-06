b1340281df5488f6d4f061f5d56401eaed38a4a3
Gaurav kumar <indiagauravkumar@gmail.com>
2025-02-07 04:51:02 +0530

    feat: add Loader component for loading state indication

---
+++ components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-[300px]">
+      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900" />
+    </div>
+  );
+};
+
+export default Loader;
